0929ac0bec7f7adffbad2f29f72ca0cd75f05bd0
Mary <you@example.com>
2023-11-20 21:58:46 +0400

    fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React, { useEffect } from 'react'
+import './index.scss'
+import { useState } from 'react'
+import { Success } from './components/Success'
+import { Users } from './components/Users'
+import Axios from 'axios'
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [invites, setInvites] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  const [searchValue, setSearchValue] = useState('')
+
+  useEffect(() => {
+    Axios.get('https://reqres.in/api/users')
+      .then((res) => {
+        setUsers(res.data.data)
+      })
+      .catch((err) => {
+        console.warn(err)
+        alert('Failed to get the username ')
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value)
+  }
+
+  const onClickInvite = (id) => {
+if (invites.includes(id) {
+  setInvites(prev => prev.filter())
+})
+  }
   return (
     <div className="App">
-      <Users />
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}
+      />
       {/* <Success /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Skeleton } from './Skeleton';
-import { User } from './User';
+import React from 'react'
+import { Skeleton } from './Skeleton'
+import { User } from './User'
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({
+  items,
+  isLoading,
+  searchValue,
+  onChangeSearchValue,
+}) => {
   return (
     <>
+      <div className="search">{/* ... */}</div>{' '}
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input
+          value={searchValue}
+          onChange={onChangeSearchValue}
+          type="text"
+          placeholder="Найти пользователя..."
+        />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -19,10 +30,18 @@ export const Users = ({ items, isLoading }) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {items
+            .filter((user) => {
+              const fullName = (user.first_name + user.last_name).toLowerCase()
+              return fullName.includes(searchValue.toLowerCase()) ||
+                user.email.toLowerCase().includes(searchValue.toLowerCase())
+            })
+            .map((user) => (
+              <User isInvited {...user} key={user.id} />
+            ))}
         </ul>
       )}
       <button className="send-invite-btn">Отправить приглашение</button>
     </>
-  );
-};
+  )
+}
